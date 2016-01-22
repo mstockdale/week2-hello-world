89700b24f57002835eb986e628e5f429a9d87a3a
Mary Stockdale <mstockdale@mail.valenciacollege.edu>
2016-01-22 09:51:30 -0500

    Revert "Hello World with Foreign Language Choice"
    This reverts commit 1d54ae072be151a60aeee9ee33defc33f04e800a.

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,10 @@
-#Mary Stockdale
+# replace the contents of this comment with your full name
 
-#Hello World Program in Chosen Language
-print('Hello World!') 
-print('Choose which language you would like me to greet you with!') #Ask reader to choose which language
-print('1=French 2=English 3=Swedish') #Language choices
-spam = input()
-if spam == '2':
-    print('Well, Hello there new friend!') #Greeting in English
-if spam == '1':
-    print('Eh bien , il Bonjour nouvel ami!') #Greeting in French
-if spam == '3':
-    print('Tja , Hallå där ny vän!') #Greeting in Swedish
-	
+# write a program that:
+# 1. greets the user in English
+# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+# 3. displays the greeting in the chosen language
+# 4. exits
+
+# make sure that your code contains comments explaining your logic!
 
